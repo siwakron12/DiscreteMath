ac96f0a187a29f7ddea7ea56445fed4901022dde
iibboss <bosswatcharaboonbxs@gmail.com>
2026-03-01 01:08:35 +0700

    add feature stack class

--- Stack.java
+++ Stack.java
@@ -1,3 +1,24 @@
+/*สำหรับ Stack
+push()
+pop()
+peek()
+isEmpty()
+isFull()
+จัดการ error (Overflow / Underflow) */
 public class Stack {
-    
+    private int maxSize ;
+    private double[] stackArray ;
+    private int top ;
+
+    public Stack(int Size) {
+        this.maxSize = Size ;
+        this.stackArray = new double[maxSize] ;
+        this.top = -1 ;
+    }
+
+    public void push(double value) {
+        if(top < maxSize-1) {
+             stackArray[top++] += value ;
+        }
+    }
 }
